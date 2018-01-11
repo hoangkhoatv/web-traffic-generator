92c50bd9bdd5e0a0ab2dd0b833cd0a263fe85e18
CNSC <hoangkhoatv95@live.com>
2018-01-10 23:49:22 -0800

    update

--- run_file.py
+++ run_file.py
@@ -2,18 +2,21 @@
 import os
 import time
 import configFile
-_nameRun = "/web/"+configFile.fileRun+'.txt'
-i = 0
-_fileRun = open(_nameRun,'r')
-while True:
-    _time = _fileRun.readline()
-    if _time == '':
-         _fileRun.close
-         break
-    _number = _fileRun.readline()
-    ran = int(_number.strip())
-    for x in range(0,ran):
-        print _fileRun.readline()
-        os.system(_fileRun.readline())
-    time.sleep(_time)
+_nameRun = configFile.fileRun+'.txt'
+for x in range(1,10):
+        os.system('docker start ubuntu'+ str(x))
+with open(_nameRun,'r') as _fileRun:
+	while True:
+
+    		_time = _fileRun.readline()
+		print "TIME: " + _time
+    		if _time == '': 
+         		break
+    		_number = _fileRun.readline()
+    		ran = int(_number.strip())
+		print "IP: " + _number
+    		for x in range(0,ran+1):
+        		print _fileRun.readline()
+       			os.system(_fileRun.readline())
+    		time.sleep(float(_time))
 
