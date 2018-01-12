f201a36ab5c43d82866760ab2188c3e6053788b8
CNSC <hoangkhoatv95@live.com>
2018-01-12 00:52:37 -0800

    update

--- sendMail.py
+++ sendMail.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # coding: utf-8
-import sys, getopt
+import sys, getopt, os
 import smtplib
 from email.Utils import formatdate
 from email.mime.text import MIMEText
@@ -8,28 +8,28 @@ from email.MIMEMultipart import MIMEMultipart
 from email.MIMEBase import MIMEBase
 from email import Encoders
 def main(argv):
-    me = ''
-    to = ''
-    content = ''
-    mfile = ''
-    try:
-        opts, args = getopt.getopt(argv,"hc:m:t:c:f",["me=","to=","content=","file="])
-    except getopt.GetoptError:
-        print 'sendMail.py -m <me> -t <to>'
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print 'sendMail.py -m <me> -t <to>'
-            sys.exit()
-        elif opt in ("-m", "--me"):
-            me = arg
-        elif opt in ("-t", "--to"):
-            to = arg
-        elif opt in ("-c", "--content"):
-            content = arg
-        elif opt in ("-f", "--file"):
-            mfile = arg
-    msg = MIMEMultipart()
+	me = ''
+	to = ''
+	content = ''
+	mfile = ''
+	try:
+		opts, args = getopt.getopt(argv,"hc:m:t:c:f",["me=","to=","content=","file="])
+	except getopt.GetoptError:
+        	print 'sendMail.py -m <me> -t <to>'
+        	sys.exit(2)
+    	for opt, arg in opts:
+        	if opt == '-h':
+            		print 'sendMail.py -m <me> -t <to>'
+            		sys.exit()
+        	elif opt in ("-m", "--me"):
+            		me = arg
+        	elif opt in ("-t", "--to"):
+            		to = arg
+        	elif opt in ("-c", "--content"):
+            		content = arg
+        	elif opt in ("-f", "--file"):
+            		mfile = arg
+    	msg = MIMEMultipart()
 	msg['From'] = me
 	msg['To'] = to
 	msg['Subject'] = 'Subject'
@@ -47,7 +47,7 @@ def main(argv):
 	msg.attach(part)
 	s = smtplib.SMTP('192.168.100.251')
 	s.login(me, 'Cnsc12345')
-	s.sendmail(me, you, msg.as_string())
+	s.sendmail(me, to, msg.as_string())
 	s.quit()
 if __name__ == "__main__":
    main(sys.argv[1:])
